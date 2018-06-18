74514f36d1e57f2fbe6f702642c37cb31ce7327b
Raphael <collingwood.raphael@gmail.com>
2018-06-18 05:36:21 -0400

    more css

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
 
   app.get("/", function(req, res) {
 
-  //affiche tous les articles en BDD
+  //affiche tous les articles en BDD en GET car ne passe pas de paramètre par la route et on ne récupère pas de données via un formulaire
   app.db.collection("articles").find({}, {"sort" : ['datefield', 'asc']} ).toArray(function (error, results) {
         if (error) throw error;
         //console.log(results);
